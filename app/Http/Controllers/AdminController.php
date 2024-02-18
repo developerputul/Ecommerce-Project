c27d57d0adf348958914c4d016df5958ae66db9b
developerputul <putulsentongchangya7@gmail.com>
2024-02-19 00:44:09 +0600

    Admin Profile  And Login  Section Updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Arr;
 
 class AdminController extends Controller
 {
@@ -32,5 +33,32 @@ class AdminController extends Controller
         $adminData = User::find($id);
         return view('admin.admin_profile_view', compact('adminData'));
     } // end method
+
+    public function AdminProfileStore(Request $request){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+
+        if($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/'.$data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['photo'] = $filename;
+        }
+        $data->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    } //end method
 
 }
--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -1,8 +1,9 @@
 @extends('admin.admin_dashboard')
 @section('admin')
 
-<div class="page-content">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
+<div class="page-content">
     <!--breadcrumb-->
     <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
         <div class="breadcrumb-title pe-3">Admin User Profile</div>
@@ -14,18 +15,6 @@
                     <li class="breadcrumb-item active" aria-current="page">Admin Profilep</li>
                 </ol>
             </nav>
-        </div>
-        <div class="ms-auto">
-            <div class="btn-group">
-                <button type="button" class="btn btn-primary">Settings</button>
-                <button type="button" class="btn btn-primary split-bg-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown">	<span class="visually-hidden">Toggle Dropdown</span>
-                </button>
-                <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg-end">	<a class="dropdown-item" href="javascript:;">Action</a>
-                    <a class="dropdown-item" href="javascript:;">Another action</a>
-                    <a class="dropdown-item" href="javascript:;">Something else here</a>
-                    <div class="dropdown-divider"></div>	<a class="dropdown-item" href="javascript:;">Separated link</a>
-                </div>
-            </div>
         </div>
     </div>
     <!--end breadcrumb-->
@@ -50,12 +39,20 @@
                                     <span class="text-secondary">codervent</span>
                                 </li>
                             </ul>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook me-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
+                                    <span class="text-secondary">codervent</span>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-8">
                     <div class="card">
                         <div class="card-body">
+                            <form method="post" action="{{ route('admin.profile.store') }}" enctype="multipart/form-data" >
+                            @csrf
                             <div class="row mb-3">
                                 <div class="col-sm-3">
                                     <h6 class="mb-0">User Name</h6>
@@ -69,7 +66,7 @@
                                     <h6 class="mb-0">Full Name</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="{{$adminData->name}}" />
+                                    <input type="text" name="name" class="form-control" value="{{$adminData->name}}" />
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -77,7 +74,7 @@
                                     <h6 class="mb-0">Email</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="{{$adminData->email}}" />
+                                    <input type="email" name="email" class="form-control" value="{{$adminData->email}}" />
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -85,7 +82,7 @@
                                     <h6 class="mb-0">Phone</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="{{$adminData->phone}}" />
+                                    <input type="text" name="phone" class="form-control" value="{{$adminData->phone}}" />
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -93,34 +90,57 @@
                                     <h6 class="mb-0">Address</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="{{$adminData->address}}" />
+                                    <input type="text" name="address" class="form-control" value="{{$adminData->address}}" />
                                 </div>
                             </div>
+
                             <div class="row mb-3">
                                 <div class="col-sm-3">
                                     <h6 class="mb-0">Photo</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="file" class="form-control"/>
+                                    <input type="file" name="photo" class="form-control" id="image"/>
                                 </div>
                             </div>
 
-
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0"></h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    <img id="showImage" src="{{ (!empty($adminData->photo)) ? url('upload/admin_images/'. $adminData->photo): url('upload/no_image.jpg')}}" alt="Admin" style="width:100px; height:100px;">
+                                    {{-- <img id="showImage" class="rounded avatar-lg" alt="Admin" src="{{ asset($adminData->photo) ?? asset('upload/no_image.jpg')}}" data-holder-rendered="true" style="width: 100px; height:100px;"> --}}
+                                </div>
+                            </div>
 
                             <div class="row">
                                 <div class="col-sm-3"></div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="button" class="btn btn-primary px-4" value="Save Changes" />
+                                    <input type="Submit" class="btn btn-primary px-4" value="Save Changes" />
                                 </div>
                             </div>
                         </div>
-                    </div>
 
+                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+$(document).ready(function(){
+    $('#image').change(function(e){
+        var reader = new FileReader();
+        reader.onload = function(e){
+            $('#showImage').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files['0']);
+    });
+});
+
+</script>
 
 
 @endsection
